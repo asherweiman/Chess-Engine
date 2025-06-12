4ad8c514608e638fa41aba6c838d09d57dafa877
asherweiman <asherweiman@gmail.com>
2025-06-12 00:22:19 -0400

    add soem basic chess piece logic for rooks, pawns, bishops, knights

--- include/main.hpp
+++ include/main.hpp
@@ -1,9 +1,17 @@
 enum chess_piece {empty = 0, pawn = 1, bishop = 2, knight = 3, rook = 4, queen = 5, king = 6};
 enum chess_team {none = 0, black = 1, white = 2};
 
+#define BOARD_LENGTH 8
+
 struct square {
 	chess_piece piece;
 	chess_team team;
 };
+
+struct pos {
+	char row;
+	char col;
+};
+
 
 void print_board ();
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.hpp"
 #include <array>
+#include <vector>
 #include <iostream>
 
 std::array<std::array<square, 8>, 8> board;
@@ -35,6 +36,234 @@ int main (int argc, char* argv[]) {
 	board[7][7] = {rook, black};
 	print_board();
 }
+
+std::vector<square> get_RowCol_moves(pos src, square src_square, std::vector<square> all_moves);
+
+// calculate all valid moves for a piece then see if dst is one of them
+bool validate_move(pos dst, pos src) {
+
+    square dst_square = board[dst.row][dst.col];
+    square src_square = board[src.row][src.col];
+
+    bool valid_move = true;
+
+    std::vector<square> all_moves;
+    switch (src_square.piece)
+    {
+    case pawn: //TODO: add en passant, promoting, this only works for one side
+        {
+            if (board[src.row+1][src.col].piece == empty)
+                all_moves.push_back(board[src.row+1][src.col]);
+
+            if (src_square.team  == black) {
+                if (board[src.row + 1][src.col + 1].team != src_square.team ) {
+                    all_moves.push_back(board[src.row+1][src.col+1]); 
+                }
+                
+                if (board[src.row + 1][src.col - 1].team != src_square.team ) {
+                    all_moves.push_back(board[src.row + 1][src.col - 1]); 
+                }
+            } else if (src_square.team == white) {
+                if (board[src.row - 1][src.col + 1].team != src_square.team ) {
+                    all_moves.push_back(board[src.row - 1][src.col + 1]); 
+                }
+                
+                if (board[src.row - 1][src.col - 1].team != src_square.team ) {
+                    all_moves.push_back(board[src.row - 1][src.col - 1]); 
+                }
+            }
+        }
+        break;
+    
+    case bishop:
+        {
+            int col = src.col - 1;
+            int row = src.row - 1;
+            while(row > 0 && col > 0) { // left upper
+                if (board[row][col].team != none) {
+                    if (board[row][col].team != src_square.team) {
+                        all_moves.push_back(board[row][col]);
+                    }
+
+                    break;
+                }
+
+                all_moves.push_back(board[row][col]);
+                row--;
+                col--;
+            }
+
+            col = src.col + 1;
+            row = src.row - 1;
+            while(row > 0 && col < BOARD_LENGTH) { // right upper
+                if (board[row][col].team != none) {
+                    if (board[row][col].team != src_square.team) {
+                        all_moves.push_back(board[row][col]);
+                    }
+
+                    break;
+                }
+
+                all_moves.push_back(board[row][col]);
+                row--;
+                col++;
+            }
+
+            col = src.col - 1;
+            row = src.row + 1;
+            while(row > 0 && col < BOARD_LENGTH) { //left lower
+                if (board[row][col].team != none) {
+                    if (board[row][col].team != src_square.team) {
+                        all_moves.push_back(board[row][col]);
+                    }
+
+                    break;
+                }
+
+                all_moves.push_back(board[row][col]);
+                row++;
+                col--;
+            }
+
+            col = src.col + 1;
+            row = src.row + 1;
+            while(row > 0 && col < BOARD_LENGTH) { //right lower
+                if (board[row][col].team != none) {
+                    if (board[row][col].team != src_square.team) {
+                        all_moves.push_back(board[row][col]);
+                    }
+
+                    break;
+                }
+
+                all_moves.push_back(board[row][col]);
+                row++;
+                col++;
+            }
+        }
+        
+        break;
+
+    case knight: // theres probably a smarter way to do this
+        {
+        if (board[src.row + 1][src.col + 2].team != src_square.team) 
+            all_moves.push_back(board[src.row + 1][src.col + 2]);
+        
+        if (board[src.row + 2][src.col + 1].team != src_square.team) 
+            all_moves.push_back(board[src.row + 2][src.col + 1]);
+        
+        if (board[src.row - 1][src.col + 2].team != src_square.team) 
+            all_moves.push_back(board[src.row - 1][src.col + 2]);
+        
+        if (board[src.row - 2][src.col + 1].team != src_square.team) 
+            all_moves.push_back(board[src.row - 2][src.col + 1]);
+        
+        if (board[src.row + 1][src.col - 2].team != src_square.team) 
+            all_moves.push_back(board[src.row + 1][src.col - 2]);
+        
+        if (board[src.row + 2][src.col - 1].team != src_square.team) 
+            all_moves.push_back(board[src.row + 2][src.col - 1]);
+        
+        if (board[src.row - 1][src.col - 2].team != src_square.team) 
+            all_moves.push_back(board[src.row - 1][src.col - 2]);
+        
+        if (board[src.row - 2][src.col - 1].team != src_square.team) 
+            all_moves.push_back(board[src.row - 2][src.col - 1]);
+
+        }
+        break;
+
+    case rook:
+        {
+            all_moves = get_RowCol_moves(src, src_square, all_moves);
+        }
+        break;
+    case queen:
+        
+
+    
+        break;
+    
+    case king:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+    return valid_move;
+
+
+}
+
+std::vector<square> get_RowCol_moves(pos src, square src_square, std::vector<square> all_moves) {
+
+        int col = src.col + 1;
+        int row = src.row;
+       while(col < BOARD_LENGTH) { // row right
+            if (board[row][col].team != none) {
+                if (board[row][col].team != src_square.team) {
+                    all_moves.push_back(board[row][col]);
+                }
+
+                break;
+            }
+
+            all_moves.push_back(board[row][col]);
+            col++;
+        }
+
+        col = src.col - 1;
+        row = src.row;
+       while(col > 0) { // row left
+            if (board[row][col].team != none) {
+                if (board[row][col].team != src_square.team) {
+                    all_moves.push_back(board[row][col]);
+                }
+
+                break;
+            }
+
+            all_moves.push_back(board[row][col]);
+            col--;
+        }
+
+        col = src.col;
+        row = src.row - 1;
+       while(row > 0) { //col upper
+            if (board[row][col].team != none) {
+                if (board[row][col].team != src_square.team) {
+                    all_moves.push_back(board[row][col]);
+                }
+
+                break;
+            }
+
+            all_moves.push_back(board[row][col]);
+            row--;
+        }
+
+        col = src.col;
+        row = src.row + 1;
+       while(row < BOARD_LENGTH) { // col lower
+            if (board[row][col].team != none) {
+                if (board[row][col].team != src_square.team) {
+                    all_moves.push_back(board[row][col]);
+                }
+
+                break;
+            }
+
+            all_moves.push_back(board[row][col]);
+            row++;
+        }
+
+    return all_moves;
+
+}
+
+
 
 // Print the current board
 void print_board () {
